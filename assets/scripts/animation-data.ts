4d500e584cd12a4ffc2d0a22c3966e42d8de8601
Yun Hsiao Wu <yunhsiaow@gmail.com>
2021-07-11 00:22:41 +0800

    better data interface

--- assets/scripts/animation-data.ts
+++ assets/scripts/animation-data.ts
@@ -18,99 +18,96 @@ export enum PlaneStates {
 }
 ccenum(PlaneStates);
 
-@ccclass('SequenceAnimationInfo')
-export class SequenceAnimationInfo {
-    @property(Texture2D)
-    texture: Texture2D = null;
-
-    params = new Vec4(4.1, 4, 0, 0);
-    playbackSpeed = 1;
-    nextState = -1;
-}
-
 enum PlaybackMode {
     LOCAL_LOOP,
     LOCAL_ONCE,
     GLOBAL_LOOP,
 }
 
+@ccclass('SequenceAnimationInfo')
+export class SequenceAnimationInfo {
+    @property(Texture2D)
+    texture: Texture2D = null;
+
+    params = new Vec4(4.1, 4, PlaybackMode.LOCAL_LOOP, 0);
+    playbackSpeed = 1;
+    nextState = -1;
+}
+
 interface IAnimationInfo {
     width: number;
     height: number;
-    frames: number;
-    mode: PlaybackMode;
-    progress: number;
-    playbackSpeed: number;
-    nextState: number;
+    frames?: number;        // default to (w * h)
+    mode?: PlaybackMode;    // default to LOCAL_LOOP
+    startFrom?: number;     // default to 0
+    playbackSpeed?: number; // default to 1
+    nextState?: number;     // default to none
 }
 
 const characterAnimInfoMap: Record<CharacterStates, IAnimationInfo> = {
     [CharacterStates.RUNNING]: {
-        width: 4, height: 1, frames: 4,
-        mode: PlaybackMode.GLOBAL_LOOP, progress: 0,
-        playbackSpeed: 1,
-        nextState: -1,
+        width: 4, height: 1,
+        mode: PlaybackMode.GLOBAL_LOOP,
     },
     [CharacterStates.JUMPING]: {
-        width: 4, height: 1, frames: 4,
-        mode: PlaybackMode.GLOBAL_LOOP, progress: 0,
-        playbackSpeed: 1,
-        nextState: -1,
+        width: 4, height: 1,
+        mode: PlaybackMode.GLOBAL_LOOP,
     },
     [CharacterStates.SLIDING]: {
-        width: 5, height: 1, frames: 5,
-        mode: PlaybackMode.LOCAL_ONCE, progress: 0,
-        playbackSpeed: 1,
-        nextState: 3,
+        width: 5, height: 1,
+        mode: PlaybackMode.LOCAL_ONCE,
+        nextState: CharacterStates.SLIDING_LOOP,
     },
     [CharacterStates.SLIDING_LOOP]: {
-        width: 2, height: 1, frames: 2,
-        mode: PlaybackMode.LOCAL_LOOP, progress: 0,
-        playbackSpeed: 1,
-        nextState: -1,
+        width: 2, height: 1,
+        mode: PlaybackMode.LOCAL_LOOP,
     },
     [CharacterStates.GLIDING]: {
-        width: 2, height: 2, frames: 4,
-        mode: PlaybackMode.LOCAL_ONCE, progress: 0,
+        width: 2, height: 2,
+        mode: PlaybackMode.LOCAL_ONCE,
         playbackSpeed: 2,
-        nextState: 5,
+        nextState: CharacterStates.GLIDING_LOOP,
     },
     [CharacterStates.GLIDING_LOOP]: {
         width: 5, height: 4, frames: 19,
-        mode: PlaybackMode.LOCAL_LOOP, progress: 0,
+        mode: PlaybackMode.LOCAL_LOOP,
         playbackSpeed: 2,
-        nextState: -1,
     },
 };
 
 const planeAnimInfoMap: Record<PlaneStates, IAnimationInfo> = {
     [PlaneStates.HIDDEN]: {
-        width: 3, height: 6, frames: 18,
-        mode: PlaybackMode.LOCAL_ONCE, progress: 18,
+        width: 3, height: 6,
+        mode: PlaybackMode.LOCAL_ONCE,
+        startFrom: 1,
         playbackSpeed: 0,
-        nextState: -1,
     },
     [PlaneStates.GLIDING_START]: {
-        width: 3, height: 6, frames: 18,
-        mode: PlaybackMode.LOCAL_ONCE, progress: 18,
+        width: 3, height: 6,
+        mode: PlaybackMode.LOCAL_ONCE,
+        startFrom: 1,
         playbackSpeed: -4,
-        nextState: -1,
     },
     [PlaneStates.GLIDING_END]: {
-        width: 3, height: 6, frames: 18,
-        mode: PlaybackMode.LOCAL_ONCE, progress: 0,
+        width: 3, height: 6,
+        mode: PlaybackMode.LOCAL_ONCE,
         playbackSpeed: 4,
-        nextState: -1,
     },
 };
 
 function fillParams (src: IAnimationInfo, dst: SequenceAnimationInfo) {
+    const speed = src.playbackSpeed !== undefined ? src.playbackSpeed : 1;
+    const mode = src.mode !== undefined ? src.mode : PlaybackMode.LOCAL_LOOP;
+    const frames = src.frames !== undefined ? src.frames : src.width * src.height;
+    const startFrom = src.startFrom !== undefined ? src.startFrom * frames : 0;
+    const nextState = src.nextState !== undefined ? src.nextState : -1;
+
     dst.params.x = src.width + src.height * 0.1;
-    dst.params.y = src.frames;
-    dst.params.z = src.mode;
-    dst.params.w = src.mode === PlaybackMode.GLOBAL_LOOP ? src.playbackSpeed : src.progress;
-    dst.playbackSpeed = src.mode === PlaybackMode.GLOBAL_LOOP ? 0 : src.playbackSpeed;
-    dst.nextState = src.nextState;
+    dst.params.y = frames;
+    dst.params.z = mode;
+    dst.params.w = mode === PlaybackMode.GLOBAL_LOOP ? speed : startFrom;
+    dst.playbackSpeed = mode === PlaybackMode.GLOBAL_LOOP ? 0 : speed;
+    dst.nextState = nextState;
 }
 
 export function fillCharacterParams (infos: SequenceAnimationInfo[]) {
