1b60315c750b2e481bdb81ff247f91f97202f93f
jiaxin.lai <goodlaijiaxin@qq.com>
2021-07-11 12:46:52 +0800

    add follow camera

--- assets/scripts/CameraClamping.ts
+++ assets/scripts/CameraClamping.ts
@@ -3,7 +3,6 @@ import { _decorator, Component, Node, Vec3, director, Director } from 'cc';
 const { ccclass, property } = _decorator;
 
 const v3_1 = new Vec3();
-const v3_2 = new Vec3();
 
 @ccclass('CameraClamping')
 export class CameraClamping extends Component {
@@ -11,16 +10,35 @@ export class CameraClamping extends Component {
     @property
     minY = 0;
 
-    start () {
-        const initialLocalY = this.node.position.y;
-        director.on(Director.EVENT_BEFORE_DRAW, () => {
-            this.node.getPosition(v3_1);
-            v3_1.y = Math.min(v3_1.y, initialLocalY);
-            this.node.setPosition(v3_1);
+    @property
+    increment = 0.05;
+    
+    @property(Node)
+    target: Node = null!;
 
-            this.node.getWorldPosition(v3_1);
-            v3_1.y = Math.max(v3_1.y, this.minY);
-            this.node.setWorldPosition(v3_1);
-        });
+    _offset = new Vec3();
+
+    start () {
+        Vec3.subtract(this._offset, this.node.worldPosition, this.target.worldPosition);
+    }
+
+    onEnable () {
+        director.on(Director.EVENT_BEFORE_DRAW, this.followTarget, this);
+    }
+
+    onDisable () {
+        director.off(Director.EVENT_BEFORE_DRAW, this.followTarget, this);
+    }
+
+    followTarget () {
+        const wp = this.target.worldPosition;
+        let z = this.node.worldPosition.z;
+        if (z - wp.z > this._offset.z) {
+            z = wp.z + this._offset.z;
+        }
+        z -= this.increment;
+        v3_1.set(wp.x, wp.y, z).add3f(this._offset.x, this._offset.y, 0)
+        v3_1.y = Math.max(v3_1.y, this.minY);
+        this.node.worldPosition = v3_1;
     }
 }
